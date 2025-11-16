e51dce9212fb8fcd7458f6398b1a2f32efb103fd
altmoket <leandro.nunez1457@gmail.com>
2025-11-15 22:53:21 -0300

    Dockerize

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { router } from './routes';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
 app.use('/api', router);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
